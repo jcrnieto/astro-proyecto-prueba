d64cdb6a6dbf4fd1fdbfc20748fd9e1ba87362e6
juan cruz <jcrnietos@gmail.com>
2024-02-15 20:27:06 -0300

    creacion del formulario

---
+++ src/data/dataCard.js
@@ -0,0 +1,22 @@
+const dataCard = [
+    {
+        id:1,
+        icon:"dfbdfb",
+        title:"Comunity Manager",
+        description:"Potenciamos tu presencia online con estrategias de marketing digital efectivas. Te ayudamos a llegar a tu audiencia de manera eficiente"
+    },
+    {
+        id:1,
+        icon:"dfbdfb",
+        title:"Social Media",
+        description:"Potenciamos tu presencia online con estrategias de marketing digital efectivas. Te ayudamos a llegar a tu audiencia de manera eficiente"
+    },
+    {
+        id:1,
+        icon:"dfbdfb",
+        title:"Creador de contenido",
+        description:"Potenciamos tu presencia online con estrategias de marketing digital efectivas. Te ayudamos a llegar a tu audiencia de manera eficiente"
+    }
+]
+
+export default dataCard;
