102b84052d78bd5d865545e5677f5203f45c295b
juan cruz <jcrnietos@gmail.com>
2024-02-21 21:17:47 -0300

    creacion de card en service

---
+++ src/data/planes.js
@@ -0,0 +1,38 @@
+const planes = [
+    {
+        title:"Plan Basico",
+        servicios:"servicios de publicacion en redes sociales",
+        posteos:"Máximo 12 posteos mensuales",
+        historias:"16 historias mensuales",
+        copy:"Copy y #",
+        estrategia:"Analisis de estrategia y contenido",
+        mercado:"Estudio de mercado y competencia",
+        call:"Generacion de call to action"
+    },
+    {
+        title:"Plan Basico",
+        servicios:"servicios de publicacion en redes sociales",
+        posteos:"Máximo 12 posteos mensuales",
+        historias:"16 historias mensuales",
+        copy:"Copy y #",
+        estrategia:"Analisis de estrategia y contenido",
+        mercado:"Estudio de mercado y competencia",
+        call:"Generacion de call to action",
+        contenido:"Generaciony edicion de contenido",
+        publicidad:"Armado y gestion de publicidad en redes sociales y Google ADS"
+    },
+    {
+        title:"Plan Basico",
+        servicios:"servicios de publicacion en redes sociales",
+        posteos:"Máximo 12 posteos mensuales",
+        historias:"16 historias mensuales",
+        copy:"Copy y #",
+        estrategia:"Analisis de estrategia y contenido",
+        mercado:"Estudio de mercado y competencia",
+        call:"Generacion de call to action",
+        contenido:"Generaciony edicion de contenido",
+        publicidad:"Armado y gestion de publicidad en redes sociales y Google ADS"
+    }
+]
+
+export default planes;
