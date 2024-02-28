6fc1e078c3b520b3aef88c87d652272139f48472
juan cruz <jcrnietos@gmail.com>
2024-02-28 19:02:14 -0300

    creacion de componente proceso

---
+++ src/data/procesos.js
@@ -0,0 +1,29 @@
+const procesos = [
+    {
+        id:1,
+        title:"Descubrir",
+        description:"Exploramos el universo digital para identificar oportunidades únicas y desafíos potenciales. Con un enfoque estratégico, nuestra fase de descubrimiento es la base de cada proyecto, donde desentrañamos las necesidades y metas del cliente."
+    },
+    {
+        id:2,
+        title:"Definir",
+        description:"Refinamos y clarificamos la visión del proyecto, estableciendo metas concretas y estrategias específicas. En esta etapa, transformamos la información recopilada en una hoja de ruta clara que guiará todo el proceso de desarrollo y marketing."
+    },
+    {
+        id:3,
+        title:"Diseñar",
+        description:"Nuestro equipo creativo da vida a las ideas a través de diseños innovadores y atractivos. Desde la experiencia del usuario hasta la identidad visual, creamos una representación visual de la visión del cliente, asegurando un diseño que no solo sea estéticamente agradable, sino también funcional."
+    },
+    {
+        id:4,
+        title:"Desarrollar",
+        description:"Convertimos los conceptos y diseños en realidad, utilizando tecnologías de última generación. La fase de desarrollo implica codificación experta y programación sólida para construir soluciones web robustas y eficientes."
+    },
+    {
+        id:5,
+        title:"Desplegar",
+        description:"Lanzamos las soluciones desarrolladas en el entorno en vivo, asegurando una transición sin problemas. Nuestro enfoque cuidadoso garantiza que cada componente esté completamente funcional y listo para ser utilizado por el público objetivo."
+    },
+]
+
+export default procesos;
